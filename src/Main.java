5b2969a5f756f8d75feca26fff6659a912360930
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2023-09-14 17:04:52 +0700

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100; // сумма начального счёта
-        int b = 2500; // сумма пополнения
-        int c = 1000; // предел ниже которого бонусы не начисляются
-        int d = 100; // 1ед. бонуса со ста рублей
-        if (b >= c) {
+        int balance = 100; // сумма начального счёта
+        int replenishment = 2500; // сумма пополнения
+        int limit = 1000; // предел ниже которого бонусы не начисляются
+        int bonus = 100; // 1ед. бонуса со ста рублей
+        if (replenishment >= limit) {
 
             System.out.println("Бонусное начисление с пополнения счёта");
-            System.out.println((b / d) + " Число полученных бонусов");
-            System.out.println((a + b) + (b / d) +" Итоговая сумма на счету клиента");
+            System.out.println((replenishment / bonus) + " Число полученных бонусов");
+            System.out.println((balance + replenishment) + (replenishment / bonus) +" Итоговая сумма на счету клиента");
         } else {
             System.out.println("Нет бонусного начисления с пополнения счёта");
-            System.out.println((a + b) + " Итоговая сумма без начисления бонусов");
+            System.out.println((balance + replenishment) + " Итоговая сумма без начисления бонусов");
         }
     }
 }
